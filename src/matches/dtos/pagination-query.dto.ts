bdd173d7984c19b5d0d07eaefe3ad4c259653f73
Victor Almeida de Freitas <victor.freitas08@hotmail.com>
2025-08-10 22:39:35 -0300

    create pagination query dto

---
+++ src/matches/dtos/pagination-query.dto.ts
@@ -0,0 +1,29 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsInt, IsOptional, Max, Min } from 'class-validator';
+
+export class PaginationQueryDto {
+  @ApiPropertyOptional({
+    example: 1,
+    description: 'Número da página',
+    minimum: 1,
+  })
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page = 1;
+
+  @ApiPropertyOptional({
+    example: 10,
+    description: 'Quantidade de itens por página',
+    minimum: 1,
+    maximum: 100,
+  })
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(100)
+  limit = 10;
+}
